c97d7d59d6040bbad09432e05cd0565f1d15b709
oclockvn <oclockvn@gmail.com>
2018-12-26 19:52:16 +0700

    update binding

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 // import * as ko from './knockout-3.4.2';
 
-(function(){
-    // class Receipt {
-    //     constructor() {
-    //         this.total = ko.observable('');
-    //         this.driver = ko.observable('');
-    //         this.code = ko.observable('');
-    //         this.pickup = ko.observable('');
-    //         this.destination = ko.observable('');
-    //         this.day = ko.observable('');
-    //         this.month = ko.observable('');
-    //         this.year = ko.observable(18);
-    //         this.time = ko.observable('00:00');
-    //     }
-    // }
+(function(){    
+    const validDate = dateString => moment(dateString, ['DD/MM/YYYY'], true).isValid();  
+    const validTime = dateString => moment(dateString, ['HH:mm'], true).isValid();  
 
     let receipt = {
         total: ko.observable(''),
         driver: ko.observable(''),
-        code: ko.observable(''),
+        code: ko.observable('ADR-'),
         pickup: ko.observable(''),
         destination: ko.observable(''),
-        datetime: ko.observable('')
+        date: ko.observable(''),
+        time: ko.observable(''),       
     };
+
+    // 08 Nov 18 15:03 +0700
+    receipt.dateFormatted = ko.computed(() => {
+        const dateInput = receipt.date();
+        const date = validDate(dateInput) ? moment(dateInput, 'DD/MM/YYYY').format('DD MMM YY') : dateInput;
+        
+        const timeInput = receipt.time();
+        const time = validTime(timeInput) ? moment(timeInput, 'HH:mm').format('HH:mm') : timeInput;
+
+        return `${date} ${time} +0700`;
+    }, receipt);
 
     ko.applyBindings(receipt);
 })();
